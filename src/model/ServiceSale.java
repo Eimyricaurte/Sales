3d7d206500eb7151e5c552aa733f2f08a187f672
Eimyricaurte <eimy.ricaurte01@uptc.edu.co>
2023-08-05 15:16:18 -0500

    new

--- src/model/ServiceSale.java
+++ src/model/ServiceSale.java
@@ -34,40 +34,4 @@ public class ServiceSale {
 		return 0;
 		
 	}
-}package model;
-
-public class ServiceSale {
-
-	public Product product;
-	public Sale nsale;
-	public int sale;
-	public ServiceSale(Product product, int sale) {
-		super();
-		this.product = product;
-		this.sale = sale;
-	}
-	public ServiceSale() {
-		
-	}
-	public int getSale() {
-		return sale;
-	}
-	public void setSale(int sale) {
-		this.sale = sale;
-	}
-	
-	public double calcIva() {
-		if (product.isIva()== true) {
-			
-			double valIva = switch(product.geteTypeProduct()) {
-			case ASEO -> nsale.calcDiscount() * 0.14;
-			case VIVERES -> nsale.calcDiscount() * 0.8;
-			case LICORES -> nsale.calcDiscount() * 0.19;
-			case RANCHO -> nsale.calcDiscount() * 0.19;
-			default -> nsale.calcDiscount();
-			};
-		}
-		return 0;
-		
-	}
 }
